0a777d615d3eda0ed1170a7442278f997b37f85a
Angela Pj <ang@Angelas-Laptop.local>
2026-01-18 14:18:18 +0800

    Fix middleware: use next-intl automatic matcher

--- Desktop/Rena.edu/src/middleware.ts
+++ Desktop/Rena.edu/src/middleware.ts
@@ -2,8 +2,3 @@ import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
 export default createMiddleware(routing);
-
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(zh-TW|en)/:path*']
-};
